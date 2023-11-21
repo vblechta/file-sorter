b31e005a19e19dcd11ccdaa250a724311d8fb534
Vojtěch Blechta <vojtech@blechta.cz>
2023-11-21 22:28:44 +0100

    added variable to change folder in file generator

--- generate_files.py
+++ generate_files.py
@@ -2,6 +2,8 @@ import os
 import random
 import shutil
 
+generate_folder_path = "data" # Replace this with the path to your folder where you want the files to be generated
+
 def generate_random_name():
     letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," "," "," "," "," "," "," "," ",]
     name = ""
@@ -12,15 +14,15 @@ def generate_random_name():
 
 def create_files():
     # Create a subdirectory named "data" if it doesn't exist
-    if not os.path.exists("data"):
-        os.makedirs("data")
+    if not os.path.exists(generate_folder_path):
+        os.makedirs(generate_folder_path)
 
     for i in range(1, 1001):
         if i % 100 == 1:
             # Create a new set of words for every 100 files
             file_begin_name = generate_random_name()
         file_end_name = generate_random_name()
-        file_name = f"data/{file_begin_name} - {file_end_name}.txt"
+        file_name = f"{generate_folder_path}/{file_begin_name} - {file_end_name}.txt"
         with open(file_name, 'w') as file:
             file.write("This is a sample file.")
 
